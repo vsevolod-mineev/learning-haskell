29ea45a4ef272046cb6c60ddc4f0129967e9f2a7
Vsevolod Mineev <vsevolod.mineev@gmail.com>
2022-01-17 00:37:09 +0000

    where-let

---
+++ src/haskell/tuples.hs
@@ -0,0 +1,7 @@
+-- 2-tuple
+tuple2 :: (Integer, String)
+tuple2 = (1, "foo")
+
+-- 3-tuple
+tuple3 :: (Integer, Integer, Integer)
+tuple3 = (10, 20, 30)
---
+++ src/haskell/where-let.hs
@@ -0,0 +1,2 @@
+l = let x = 1; y = 2 in (x+y)
+w = x+y where x=1; y=1
