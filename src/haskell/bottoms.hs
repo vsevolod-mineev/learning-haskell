25955478c86d6257526c54d9dd27318182b67ef3
Vsevolod Mineev <vsevolod.mineev@gmail.com>
2022-01-19 01:16:05 +0000

    bottoms

--- src/haskell/bottoms.hs
+++ src/haskell/bottoms.hs
@@ -11,8 +11,15 @@ head :: [a] -> a
 head (x:_) =  x
 head []    =  error "Prelude.head: empty list"
 
+--takes any element of a list
 (!!) :: [a] -> Int -> a
 xs     !! n | n < 0 =  error "Prelude.!!: negative index"
 []     !! _         =  error "Prelude.!!: index too large"
 (x:_)  !! 0         =  x
 (_:xs) !! n         =  xs !! (n-1)
+
+--safer version of head
+listToMaybe :: [a] -> Maybe a
+listToMaybe []     =  Nothing    -- An empty list returns Nothing
+listToMaybe (a:_)  =  Just a     -- A non-empty list returns the first element
+                                 -- wrapped in the Just context
