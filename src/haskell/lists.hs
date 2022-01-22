1c6fc423747be636a92831501cee5d910d8c0a98
Vsevolod Mineev <vsevolod.mineev@gmail.com>
2022-01-22 19:06:10 +0000

    lists

--- src/haskell/lists.hs
+++ src/haskell/lists.hs
@@ -1,3 +1,4 @@
+import Data.List
 [1,2,3] = 1 : 2 : 3 : []      
 [1,2,3] = 1 : (2 : (3 : [])) -- with explicit parens
 
@@ -22,3 +23,31 @@ powersOfTwo = iterate (2*) 1
     return square_list
 
 print(powersOfTwo(15)) -}
+
+--generate list
+asc :: Int -> Int -> [Int]
+asc n m
+ | m < n = []
+ | m == n = [m]
+ | m > n = n: asc (n+1) m
+
+ --Data.List
+{- 
+λ> head (asc 1 6)
+1
+
+tail
+
+length
+
+λ> init (asc 1 6)
+[1,2,3,4,5]
+
+λ> null (asc 1 6)
+False
+ -}
+
+ --List Comprehension
+compr = [2*x | x <- [1,2,3]]
+compr2 = [2*x | x <- [1,2,3], x > 1]
+compr3 = [(x,y) | x <- [1,2,3], y <- ['a','b']]
