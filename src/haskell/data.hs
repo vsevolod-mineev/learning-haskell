ddbf9ca079015fce8799d963af77093889281edb
Vsevolod Mineev <vsevolod.mineev@gmail.com>
2022-01-23 22:15:20 +0000

    data

---
+++ src/haskell/data.hs
@@ -0,0 +1,8 @@
+data Color = 
+    Red | Orange | Yellow | Green | Blue | Magenta
+
+data PeaNum =
+    Succ PeaNum | Zero
+
+data Calculation = 
+    Add Int Int | Sub Int Int | Mul Int Int | Div Int Int
