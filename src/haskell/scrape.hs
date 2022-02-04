d1ddd70699cc69364c8015fb85ec15b870a15698
Vsevolod Mineev <vsevolod.mineev@gmail.com>
2022-02-04 02:02:57 +0000

    broken

---
+++ src/haskell/scrape.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Text.HTML.Scalpel
+import Network.HTTP.Conduit
+import qualified Data.ByteString.Lazy as L
+import Control.Applicative
+
+url = "https://myanimelist.net/topanime.php?type=airing"
+
+openURL = simpleHttp url >>= L.putStr
+
+type Name = String
+
+newtype Anime
+    = Anime Name
+    deriving (Show, Eq)
+
+allAnime :: IO (Maybe [Anime])
+allAnime = scrapeURL "https://myanimelist.net/topanime.php?type=airing" animeNames
+   where
+       animeNames :: Scraper String [Anime]
+       animeNames = chroots ("h3" @: [hasClass "anime_ranking_h"]) name
+
+       name :: Scraper String Anime
+       name = textName
+
+       textName :: Scraper String Anime
+       textName = do
+           name      <- text $ "a" @: [hasClass "name"]
+           return $ Anime name
