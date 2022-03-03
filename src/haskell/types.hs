88d6a6425a406cdffb3ee99ccccd6f1a58136d77
Vsevolod Mineev <vsevolod.mineev@gmail.com>
2022-03-03 20:31:38 +0000

    types

---
+++ src/haskell/types.hs
@@ -0,0 +1,19 @@
+x :: Integer
+x = 5
+
+y :: Integer
+y = 0
+
+z = x + y
+
+f :: [Bool] -> Bool
+f xs = head xs
+
+c :: Char
+c = 'c'
+
+str :: String
+str = "str"
+
+str2 :: [Char]
+str2 = ['s','t','r']
